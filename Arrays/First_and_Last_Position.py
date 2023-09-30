232b9bf415eadbcb1ac0202234a7f0cda75de74b
Sahana <sahanaa2602@gmail.com>
2023-09-30 23:58:34 +0530

    occurance

--- Arrays/First_and_Last_Position.py
+++ Arrays/First_and_Last_Position.py
@@ -1,24 +1,13 @@
 def searchRange(nums, target):
-        a=[]
-        '''for num in nums:
-            if num==target :
-                x=nums.index(num)
-                a.append(x)
-                break
-        for num in nums[::-1]:
-            if num==target :
-                x=nums.index(num)
-                a.append(x)
-                break'''
+        result = []
         if target in nums:
-            a1=a.index(target)
-            a.append(a1)
-            a.reverse()
-            a2=a.index(target)
-            a.append(a2)
-            return a
+            first_index = nums.index(target)
+            result.append(first_index)
+            last_index = len(nums) - 1 - nums[::-1].index(target)
+            result.append(last_index)
+            return result
         else:
-              return [-1,-1]
+            return [-1, -1]
         
 
 nums = [5,7,7,8,8,10]
